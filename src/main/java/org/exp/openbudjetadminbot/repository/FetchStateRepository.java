075d18d53828a71b220ac2eab88e0b40f58465a1
Suhrob <suhrob_kalandarov@outlook.com>
2025-08-25 08:38:21 +0500

    Remove `FetchState` model and `FetchStateRepository` as they are no longer in use.

--- src/main/java/org/exp/openbudjetadminbot/repository/FetchStateRepository.java
+++
@@ -1,9 +0,0 @@
-package org.exp.openbudjetadminbot.repository;
-
-import org.exp.openbudjetadminbot.models.FetchState;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FetchStateRepository extends JpaRepository<FetchState, String> {
-}
